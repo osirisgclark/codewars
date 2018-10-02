e4634c864b7fca7dbd5a35d44008cc3a329b8c30
OsirisMango <osirisgclark@gmail.com>
2018-10-02 18:26:37 -0400

    Fix typo

--- 8-kyu-removespaces.py
+++ 8-kyu-removespaces.py
@@ -4,9 +4,9 @@ def no_space(x):
 
 
 
-print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))#, '8j8mBliB8gimjB8B8jlB')
-print(no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'))#, '88Bifk8hB8BB8BBBB888chl8BhBfd')
-print(no_space(('8aaaaa dddd r     '))#, '8j8mBliB8gimjB8B8jlB')
-print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))#, ''8aaaaaddddr')
-print(no_space('jfBm  gk lf8hg  88lbe8 '))#, 'jfBmgklf8hg88lbe8')
-print(no_space(('8j aam'))#, '8jaam')
+print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))  #, '8j8mBliB8gimjB8B8jlB')
+print(no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'))  #, '88Bifk8hB8BB8BBBB888chl8BhBfd')
+print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))  #, ''8aaaaaddddr')
+print(no_space('jfBm  gk lf8hg  88lbe8 '))  #, 'jfBmgklf8hg88lbe8')
+print(no_space('8j aam')  #, '8jaam')
+print(no_space('8aaaaa dddd r     ')  #, '8j8mBliB8gimjB8B8jlB')
